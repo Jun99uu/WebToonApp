796deb9b1bf4b3f8751a49fd6ca8b8842c690e2c
Jun99uu <igun0423@naver.com>
2023-02-15 15:07:36 +0900

    add : api 통신

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon/services/api_service.dart';
 import 'screens/home_screen.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const MyApp());
 }
 
